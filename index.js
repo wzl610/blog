43174a9a97f3599a0f827b343e013b3b691ef975
Allen <237984496@qq.com>
2017-01-17 17:27:38 +0800

    init project

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+let path = require('path');
 let express = require('express');
 let app = express();
 let indexRouter = require('./routes/index');
 let userRouter = require('./routes/users');
+
+app.set('views',path.join(__dirname,'views'));//设置存放模板文件的目录
+app.set('view engine','ejs');//设置模板引擎为ejs
 
 app.use('/',indexRouter);
 app.use('/users',userRouter);
--- routes/users.js
+++
@@ -1,8 +0,0 @@
-let express = require('express');
-let router = express.Router();
-
-router.get('/:name',function(req,res){
-	res.send('hello,'+req.params.name);
-});
-
-module.exports = router;
