79c95d73db680fd654463d9d04b3c623beb0326b
Allen <237984496@qq.com>
2017-01-17 18:20:51 +0800

    添加路由

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
-let express = require('express');
-let router = express.Router();
-
-router.get('/',function(req,res){
-	res.send('hello express');
-});
-
-module.exports = router;
+module.exports = function (app) {
+  app.get('/', function (req, res) {
+    res.redirect('/posts');
+  });
+  app.use('/signup', require('./signup'));
+  app.use('/signin', require('./signin'));
+  app.use('/signout', require('./signout'));
+  app.use('/posts', require('./posts'));
+};
