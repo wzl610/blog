f29e952bd98321e00211958045b5b36701328197
wzl610 <237984496@qq.com>
2017-07-07 14:46:09 +0800

    增加登录、注册路由

--- routes/router.js
+++ routes/router.js
@@ -1,4 +1,6 @@
 module.exports = function(app) {
     app.use('/', require('./index'));
     app.use('/users', require('./users'));
+    app.use('/signup',require('./signup'));//注册
+    app.use('/signin',require('./signin'));//登录
 }
